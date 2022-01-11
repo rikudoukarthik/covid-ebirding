1fbb7fc47e5e8a7d212605ee021bbe719a7b4653
Karthik Thrikkadeeri <rikudoukarthik@gmail.com>
2022-01-11 08:17:02 +0530

    binom. CI, bootstrapping, propagating SE (7/n): birding distance & dev. 2 bootstrap fns

--- boot_function_dev.R
+++ boot_function_dev.R
@@ -24,5 +24,56 @@ y <- x %>% ungroup() %>%
   summarise(NO = median(NO.SLISTS),
             NO.SE = boot_se2(NO.SLISTS, B = 100))
 
+boot_fn = function(x, fn = median, B = 100) {
+  1:B %>%
+    # For each iteration, generate a sample of x with replacement
+    map(~ x[sample(1:length(x), replace = TRUE)]) %>%
+    # Obtain the fn estimate for each bootstrap sample
+    map_dbl(fn) %>%
+    # Obtain the standard error
+    sd()
+}
 
 
+boot_fn2 = function(x, fn = median, B = 100) {
+  1:B %>%
+    # For each iteration, generate a sample of x with replacement
+    map_dbl(~ x[sample(1:length(x), replace = TRUE)]) %>%
+    # Obtain the fn estimate for each bootstrap sample
+    map_dbl(fn) %>%
+    # Obtain the standard error
+    sd()
+}
+
+set.seed(420)
+z <- x %>% ungroup() %>% 
+  group_by(COVID, YEAR, MONTH, STATE) %>% 
+  summarise(NO = median(NO.SLISTS),
+            NO.SE = boot_fn2(NO.SLISTS, B = 100))
+
+
+set.seed(57575)
+rbind(system.time(x %>% ungroup() %>% 
+                    group_by(COVID, YEAR, MONTH, STATE) %>% 
+                    summarise(NO = median(NO.SLISTS),
+                              NO.SE = boot_se2(NO.SLISTS, B = 100))),
+      system.time(x %>% ungroup() %>% 
+                    group_by(COVID, YEAR, MONTH, STATE) %>% 
+                    summarise(NO = median(NO.SLISTS),
+                              NO.SE = boot_fn(NO.SLISTS, B = 100))))
+
+
+data1 <- mtcars %>% 
+  select(gear, hp) %>% 
+  group_by(gear)
+
+data2 <- data %>% 
+  summarise(hpmed = median(hp),
+            hpse = boot_se2(hp))
+
+data3 <- data %>% 
+  summarise(hpmed = median(hp),
+            hpse = boot_fn(hp))
+
+library(microbenchmark)
+microbenchmark(data2, data3, times = 1000)
